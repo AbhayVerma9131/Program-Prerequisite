f1de325be3f82927968819a320c9f0c86c61c93d
AbhayVerma9131 <abhay45ver@gmail.com>
2025-01-08 11:19:27 +0530

    Tenth solution

---
+++ Q10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q10{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter Kilometers:");
+	double p=sc.nextDouble();
+	System.out.print("The distance in Miles is:");
+	System.out.println(p*0.621371);
+}
+}
