04a8b297a6915e79eb7940f03907a64b24e6a773
AbhayVerma9131 <abhay45ver@gmail.com>
2025-01-08 11:35:13 +0530

    update eight

--- Q8.java
+++ Q8.java
@@ -1,24 +1,5 @@
 import java.util.Scanner;
-
 public class Q8{
-
-	public static int power(int x, int y)
-	{
-		int temp;
-		if (y == 0)
-			return 1;
-		temp = power(x, y / 2);
-
-		if (y % 2 == 0)
-			return temp * temp;
-		else {
-			if (y > 0)
-				return x * temp * temp;
-			else
-				return (temp * temp) / x;
-		}
-	}
-
 
 public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
@@ -26,7 +7,7 @@ public static void main(String[] args){
 	int p=sc.nextInt();
 	System.out.print("Enter b:");
 	int r=sc.nextInt();
-	int si=power(p,r);
+	double si=Math.pow(p,r);
 	System.out.print("The power claculation is:");
 	System.out.println(si);
 }
